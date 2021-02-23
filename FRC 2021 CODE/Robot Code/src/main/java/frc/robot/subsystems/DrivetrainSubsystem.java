a233329a052561046f47ead3f129de4810ef9c6b
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2021-02-23 16:24:51 -0500

    Changed Drivetrain to subsytembase

--- FRC 2021 CODE/Robot Code/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ FRC 2021 CODE/Robot Code/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems;
 
 
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DrivetrainSubsystem implements Subsystem {
+public class DrivetrainSubsystem extends SubsystemBase {
     /**
      * The Singleton instance of this DrivetrainSubsystem. Code should use
      * the {@link #getInstance()} method to get the single instance (rather
@@ -42,9 +42,11 @@ public class DrivetrainSubsystem implements Subsystem {
      * is private since this class is a Singleton. Code should use
      * the {@link #getInstance()} method to get the singleton instance.
      */
-    public DrivetrainSubsystem() {
+    private DrivetrainSubsystem() {
         // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
         //       in the constructor or in the robot coordination class, such as RobotContainer.
+        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
     }
 }
 
